9c86bf6f08a485114a711490ac7bb3ef3e4fb531
kishore2005-k <kishoregaja.rb25@gmail.com>
2026-09-21 16:17:54 +0530

    day1

--- exercise.java
+++ exercise.java
@@ -1,5 +1,7 @@
 public class exercise{
     public static void main(String[] args){
+        //first exercise
+        //create variable for your name(strings),age(int),height(float),weight(double).print all of them
         String name="kishore";
         int age=21;
         float height=173.5f;
@@ -10,10 +12,22 @@ public class exercise{
         System.out.println("Weight="+weight);
 
         //second exercise
+        //create two integer variable a=15 and b=25.print their sum,diff,product,and division.
         int a=15;
         int b=25;
 
-        System.out.
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+
+        //third exercise
+        //create a string variables for your city name and an int variable for pincode.print both
+        String cityname="mamballam";
+        int pincode=601103;
+        System.out.println(cityname+pincode);
+
+
 
         
 
